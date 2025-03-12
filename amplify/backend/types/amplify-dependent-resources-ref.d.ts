5fd3d52af4c688e97a1f7a7ad5297062089c2e1f
Carlos <pianeta05@hotmail.com>
2025-03-12 16:50:22 +0000

    lint

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,63 +1,63 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "AdminQueries": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    },
-    "wCleanerApi": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    }
-  },
-  "auth": {
-    "userPoolGroups": {
-      "AdminGroupRole": "string"
-    },
-    "wcleaner": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
-  "function": {
-    "AdminQueries0c37162b": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "wCleanerApi": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "wcleanerCustomMessage": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  },
-  "storage": {
-    "wcleaner": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "SortKeyName": "string",
-      "SortKeyType": "string",
-      "StreamArn": "string"
-    }
-  }
-}
+  api: {
+    AdminQueries: {
+      ApiId: "string";
+      ApiName: "string";
+      RootUrl: "string";
+    };
+    wCleanerApi: {
+      ApiId: "string";
+      ApiName: "string";
+      RootUrl: "string";
+    };
+  };
+  auth: {
+    userPoolGroups: {
+      AdminGroupRole: "string";
+    };
+    wcleaner: {
+      AppClientID: "string";
+      AppClientIDWeb: "string";
+      IdentityPoolId: "string";
+      IdentityPoolName: "string";
+      UserPoolArn: "string";
+      UserPoolId: "string";
+      UserPoolName: "string";
+    };
+  };
+  function: {
+    AdminQueries0c37162b: {
+      Arn: "string";
+      LambdaExecutionRole: "string";
+      LambdaExecutionRoleArn: "string";
+      Name: "string";
+      Region: "string";
+    };
+    wCleanerApi: {
+      Arn: "string";
+      LambdaExecutionRole: "string";
+      LambdaExecutionRoleArn: "string";
+      Name: "string";
+      Region: "string";
+    };
+    wcleanerCustomMessage: {
+      Arn: "string";
+      LambdaExecutionRole: "string";
+      LambdaExecutionRoleArn: "string";
+      Name: "string";
+      Region: "string";
+    };
+  };
+  storage: {
+    wcleaner: {
+      Arn: "string";
+      Name: "string";
+      PartitionKeyName: "string";
+      PartitionKeyType: "string";
+      Region: "string";
+      SortKeyName: "string";
+      SortKeyType: "string";
+      StreamArn: "string";
+    };
+  };
+};
